a478521110d607eef2b00e2a300239f6438ba5bd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-25 08:57:24 +0300

    Task 2 : Define Multipart properties in application.yml with revision

--- src/main/java/com/example/demo/PdfCompareApplication.java
+++ src/main/java/com/example/demo/PdfCompareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PdfcompareApplication {
+public class PdfCompareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PdfcompareApplication.class, args);
+		SpringApplication.run(PdfCompareApplication.class, args);
 	}
 
 }

c5b87e0e3ab01ee159150b4cc281efd17455bc8d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-02-25 10:27:14 +0300

    Task 3 : Revise package name

--- src/main/java/com/example/pdfcompare/PdfCompareApplication.java
+++ src/main/java/com/example/pdfcompare/PdfCompareApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.pdfcompare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
